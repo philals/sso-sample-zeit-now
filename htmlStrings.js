02c847e5031dca3d3334e3c191ea14f46ede142b
Phil Alsford <phil.alsford@xero.com>
2018-11-28 07:59:13 +1300

    Updates HTML

--- htmlStrings.js
+++ htmlStrings.js
@@ -13,11 +13,11 @@ h1 {
 }
 label {
   display: block;
-}</style></head><body><h1>Zeit Xero SSO App ${process.env.MY_VARIABLE}</h1><img class="mountain-logo" src="images/mountain.png"><p><a href="login"> <img class="mountain-logo" src="images/xero_login.png"></a></p></body></html>`
+}</style></head><body><h1>Serverless Zeit Xero SSO App</h1><img class="mountain-logo" src="images/mountain.png"><p><a href="login"> <img class="mountain-logo" src="images/xero_login.png"></a></p></body></html>`
 
 export function appHtml(email) {
   return `
-<!DOCTYPE html><html><head><title>My Serverless Application</title><style>body {
+<!DOCTYPE html><html><head><title>Serverless Zeit Xero SSO App</title><style>body {
   width: 650px;
   margin: auto;
 }
@@ -34,5 +34,5 @@ p {
 }
 label {
   display: block;
-}</style></head><body><h1>Zeit Xero SSO App</h1><img class="mountain-logo" src="images/volcano.png"><p>Welcome ${email}</p></body></html>`
+}</style></head><body><h1>Serverless Zeit Xero SSO App</h1><img class="mountain-logo" src="images/volcano.png"><p>Welcome ${email}</p></body></html>`
 } 
